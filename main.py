36e8d28879fcc18ed1d8c6d242ba1f0cea59ba24
nilnix <conspicuouscontour.gmail.com>
2025-02-16 04:54:25 +0100

    updated main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("Starting asteroids!")
+    print({SCREEN_WIDTH})
+    print({SCREEN_HEIGHT})
+
+
+
+if __name__ == "__main__":
+    main()
