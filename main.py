9d16f336ada406cf79d2631959ab6e5920eaaf7c
nilnix <conspicuouscontour.gmail.com>
2025-02-16 06:33:22 +0100

    upd main.py: added player model,created player.py and circleshape.py: contains player model and hitbox

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
 
 def main():
     print("Starting asteroids!")
@@ -9,13 +12,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
         fps.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
