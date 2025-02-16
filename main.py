d5c19cf4b07905c84c2d3388e6f600b7b14de301
nilnix <conspicuouscontour.gmail.com>
2025-02-16 05:32:07 +0100

    upd main.py: added clock object

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import pygame
 from constants import *
 
-
-
-
-
-
-
-
-
-
-
-
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -25,6 +16,6 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        fps.tick(60) / 1000
 if __name__ == "__main__":
     main()
