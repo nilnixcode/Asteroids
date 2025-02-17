8a3baede819929565f5e1db0703f862a595f2145
nilnix <conspicuouscontour.gmail.com>
2025-02-17 13:33:25 +0100

    added ability for asteroid to split on collision to attack

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for bullet in shots:
                 if bullet.check_collision(asteroid):
                     bullet.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill((0, 0, 0))
 
         for sprite in drawable:
